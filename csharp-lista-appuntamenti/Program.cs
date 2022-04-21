a76a021adaab4d9fd62f78c671a8026380eb5887
erikaan <ancoraerika@gmail.com>
2022-04-21 23:55:24 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-appuntamenti/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ListaAppuntamenti;
+List<ListaAppuntamenti> listaAppuntamenti = new List<ListaAppuntamenti>();
+
+Console.WriteLine("La mia Agenda - Benvenuto!");
+Console.WriteLine("Desideri aggiungere un appuntamento? Si/No");
+
+{ int numeroDiAppuntamenti = 0;
+  bool numeroDiAppuntamentiCorretto = false;
+    while (numeroDiAppuntamentiCorretto) {
+        if (int.TryParse(Console.ReadLine(), out int result))
+        {
+            numeroDiAppuntamenti = result;
+            numeroDiAppuntamentiCorretto = true;
+        }
+        else
+        {
+            Console.WriteLine("Inserisci un numero");
+        }
+    }
+
+    for (int i = 0; i < numeroDiAppuntamenti; i++)
+    {
+        Console.WriteLine("Appuntamento numero " + (i + 1));
+        Console.WriteLine("Aggiungi il nome del tuo appuntamento: ");
+        string nomeAppuntamento = Console.ReadLine();
+        Console.WriteLine("Aggiungi il luogo del tuo appuntamento: ");
+        string localitaAppuntamento = Console.ReadLine();
+
+        bool dataCorretta = false;
+        while (!dataCorretta)
+        {
+            DateTime dataOraAppuntamento = DateTime.Now;
+            bool formatoDataCorretto = false;
+
+            while (formatoDataCorretto)
+            {
+                Console.WriteLine("Aggiungi data e ora del tuo appuntamento: [gg/mm/aaaa hh:mm]");
+
+                try
+                {
+                    dataOraAppuntamento = DateTime.Parse(Console.ReadLine());
+                    formatoDataCorretto = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Il formato della data non è corretto");
+                }
+            }
+            
+            try
+            {
+                listaAppuntamenti.Add(new Appuntamenti(nomeAppuntamento, localitaAppuntamento, dataOraAppuntamento));
+                dataCorretta = true;
+            }
+            catch (InvalidDataException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+foreach (Appuntamenti appuntamento in listaAppuntamenti)
+    {
+        appuntamento.ToString();
+    }
+
---
+++ csharp-lista-appuntamenti/listaAppuntamenti.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamenti
+    {  public class medico;
+    }
+}
